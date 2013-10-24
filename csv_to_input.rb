a0a280c8de84c8a5e2d239f9ee180fdf6b49b036
andrei512 <puni.andrei23@gmail.com>
2013-10-24 12:56:33 +0300

    normalized input and parsed new input in c into Location struct

--- csv_to_input.rb
+++ csv_to_input.rb
@@ -1,26 +1,50 @@
 require 'csv'
 
-bad = 0
-normal = 0
-lines = []
+to_convert = open(ARGV[0]).read.encode!('UTF-16', 'UTF-8', :invalid => :replace, :replace => '').encode!('UTF-8', 'UTF-16')
 
-open(ARGV[0]).each do |line|
-	csv_line = []
-  begin
-		normal += 1
-		csv_line = CSV.parse(line)
 
-		lines << csv_line 
-	rescue ArgumentError
-		line.encode!('UTF-16', 'UTF-8', :invalid => :replace, :replace => '?')
-		line.encode!('UTF-8', 'UTF-16')
+idx = 0
 
-		bad += 1
-		csv_line = CSV.parse(line)
+lines = CSV.parse(to_convert)
 
-		lines << csv_line
+File.open(ARGV[1], "w") do |file|  
+	lines.each do |row|
+		unless idx == 0			
+			file.write(row.join("\n") + "\n")			
+		else 			
+			file.write("#{lines.count - 1}\n")			
+		end		
+		idx += 1 
 	end
-	puts csv_line
 end
 
-puts "#{normal} normal lines and #{bad} bad lines"
+
+# bad = 0
+# normal = 0
+# lines = []
+
+# open(ARGV[0]).each do |line|
+# 	csv_line = []
+#   begin
+# 		normal += 1
+# 		csv_line = CSV.parse(line)
+
+# 		lines << csv_line 
+# 	rescue ArgumentError
+# 		line.encode!('UTF-16', 'UTF-8', :invalid => :replace, :replace => '?')
+# 		line.encode!('UTF-8', 'UTF-16')
+
+# 		bad += 1
+# 		csv_line = CSV.parse(line)
+
+# 		lines << csv_line
+# 	end
+# end
+
+# # puts "#{normal} normal lines and #{bad} bad lines"
+
+# File.open(ARGV[1], "w") do |file|  
+# 	lines.each do |line|
+# 		file.write(line)
+# 	end
+# end
--- maat.cpp
+++ maat.cpp
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define DBG
 
@@ -8,25 +10,85 @@
 	#define dprintf(fmt_args...) /* nothing */
 #endif
 
+
+typedef struct {
+	char *id, *name, *precise_name, *layer, *state, *country;
+} Location;
+
+
+
+int N;
+Location *locations;
+
 void load_input(char *input_file) {
 	FILE *input = fopen(input_file, "r");
 
-	char line[255];
+	fscanf(input, "%d\n", &N);
 
-	// Get column names and ignore them
-	
-	// Get all the lines
+	locations = (Location *)malloc(N * sizeof(Location));
+
+	char buffer[128];
+
+	for (int i = 0; i < N; ++i) {
+		
+		// ID
+		fgets(buffer, 255, input);
+		buffer[strlen(buffer) - 1] = 0;
+		locations[i].id = (char *)malloc((strlen(buffer) + 1) * sizeof(char));
+		memcpy(locations[i].id, buffer, strlen(buffer));
+
+		// NAME
+		fgets(buffer, 255, input);
+		buffer[strlen(buffer) - 1] = 0;
+		locations[i].name = (char *)malloc((strlen(buffer) + 1) * sizeof(char));
+		memcpy(locations[i].name, buffer, strlen(buffer));
+
+		// PRECISE NAME
+		fgets(buffer, 255, input);
+		buffer[strlen(buffer) - 1] = 0;
+		locations[i].precise_name = (char *)malloc((strlen(buffer) + 1) * sizeof(char));
+		memcpy(locations[i].precise_name, buffer, strlen(buffer));
+
+		// LAYER
+		fgets(buffer, 255, input);
+		buffer[strlen(buffer) - 1] = 0;
+		locations[i].layer = (char *)malloc((strlen(buffer) + 1) * sizeof(char));
+		memcpy(locations[i].layer, buffer, strlen(buffer));
+
+		// STATE
+		fgets(buffer, 255, input);
+		buffer[strlen(buffer) - 1] = 0;
+		locations[i].state = (char *)malloc((strlen(buffer) + 1) * sizeof(char));
+		memcpy(locations[i].state, buffer, strlen(buffer));
+
+		// COUNTRY
+		fgets(buffer, 255, input);
+		buffer[strlen(buffer) - 1] = 0;
+		locations[i].country = (char *)malloc((strlen(buffer) + 1) * sizeof(char));
+		memcpy(locations[i].country, buffer, strlen(buffer));
+	}
+
+	for (int i = 0; i < N; ++i) {
+		printf("%s %s %s %s %s %s\n", 
+			locations[i].id, 		
+			locations[i].name, 		
+			locations[i].precise_name, 		
+			locations[i].layer, 		
+			locations[i].state, 			
+			locations[i].country
+		);
+	}
 
 	fclose(input);
 }
 
 inline void query_loop() {
-	// while (true) {
-	// 	char string[100];
-	// 	scanf("%s", string);
-	// 	printf("%s\n", string);
-	// 	fflush(stdout);
-	// }
+	while (true) {
+		char string[100];
+		scanf("%s", string);
+		printf("%s\n", string);
+		fflush(stdout);
+	}
 }
 
 int main(int argc, char **args) {
@@ -38,5 +100,6 @@ int main(int argc, char **args) {
 	load_input(args[1]);
 
 	// query_loop();
+
 	return 0;
 }
